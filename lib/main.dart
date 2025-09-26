cf3541d19a7e804ea0cbb620d7f92ca982123246
Youssef Ehab <youssefhoba8@gmail.com>
2025-09-26 19:59:06 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:payment_app/features/checkout/presentation/views/my_cart_view.dart';
 
 void main() {
   runApp(const MyCartView());
 }
+// paymentIntentObject create payment intent (amount , currency)
+// init payment sheet (paymentIntentClientSecret)
+// display payment sheet
+// confirm payment sheet
