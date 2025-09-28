fcdaf474c726222b78925f114405686a66c2347f
Youssef Ehab <youssefhoba8@gmail.com>
2025-09-28 14:54:24 +0300

    add again payment cubit with flutter_bloc

--- lib/features/checkout/presentation/manger/cubit/payment_cubit.dart
+++ lib/features/checkout/presentation/manger/cubit/payment_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
-import 'package:payment_app/features/checkout/presentation/manger/payment_state.dart';
+import 'package:meta/meta.dart';
+
+part 'payment_state.dart';
 
 class PaymentCubit extends Cubit<PaymentState> {
   PaymentCubit() : super(PaymentInitial());
---
+++ lib/features/checkout/presentation/manger/cubit/payment_state.dart
@@ -0,0 +1,6 @@
+part of 'payment_cubit.dart';
+
+@immutable
+sealed class PaymentState {}
+
+final class PaymentInitial extends PaymentState {}
